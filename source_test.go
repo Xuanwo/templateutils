e18bf4efb1a09e87aa40c5806bc6f8d854496565
Xuanwo <github@xuanwo.io>
2020-05-22 11:41:23 +0800

    Add source parse support
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ source_test.go
@@ -0,0 +1,44 @@
+package templateutils
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSource_ParseContent(t *testing.T) {
+	t.Run("parse interface", func(t *testing.T) {
+		filename := "testdata/source_1"
+		content, err := ioutil.ReadFile(filename)
+		if err != nil {
+			t.Errorf("read file: %v", err)
+		}
+
+		s := &Source{}
+		err = s.ParseContent(filename, content)
+		if err != nil {
+			t.Errorf("parse content: %v", err)
+		}
+
+		assert.Equal(t, 7, len(s.Interfaces))
+	})
+
+	t.Run("parse functions", func(t *testing.T) {
+		filename := "testdata/source_2"
+		content, err := ioutil.ReadFile(filename)
+		if err != nil {
+			t.Errorf("read file: %v", err)
+		}
+
+		s := Source{}
+		err = s.ParseContent(filename, content)
+		if err != nil {
+			t.Errorf("parse content: %v", err)
+		}
+
+		for _, v := range s.Methods {
+			println(v.Name)
+		}
+	})
+}
